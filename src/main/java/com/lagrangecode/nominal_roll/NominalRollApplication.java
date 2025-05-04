72eccd2acf759f50eb34a3224279c49ad7c01ec7
lagrandecode <seunogunmolufirst1@gmail.com>
2025-05-04 17:57:50 -0400

    commit

---
+++ src/main/java/com/lagrangecode/nominal_roll/NominalRollApplication.java
@@ -0,0 +1,13 @@
+package com.lagrangecode.nominal_roll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NominalRollApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NominalRollApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lagrangecode/nominal_roll/NominalRollApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lagrangecode.nominal_roll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NominalRollApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
