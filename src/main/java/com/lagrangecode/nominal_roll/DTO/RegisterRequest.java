fb7388f286cf7347823f291ec361a090d86e0e83
lagrandecode <seunogunmolufirst1@gmail.com>
2025-05-07 23:04:29 -0400

    commit

--- src/main/java/com/lagrangecode/nominal_roll/DTO/RegisterRequest.java
+++ src/main/java/com/lagrangecode/nominal_roll/DTO/RegisterRequest.java
@@ -1,9 +1,15 @@
 package com.lagrangecode.nominal_roll.DTO;
 
 
+import com.lagrangecode.nominal_roll.Entity.StaffProfile;
+import com.lagrangecode.nominal_roll.Enums.Role;
 import lombok.Data;
 
 @Data
 public class RegisterRequest {
+    private String email;
+    private String password;
+    private Role role;
+    private StaffProfile profile;
 
 }
